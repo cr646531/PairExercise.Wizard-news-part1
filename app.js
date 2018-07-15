5a68d39104fef1a11dbb2e9aac9b5de06a0727e0
Charlie <cr646531@gmail.com>
2018-07-15 15:06:55 -0400

    WizardII

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const postBank = require('./postBank');
 const morgan = require("morgan");
 const app = express();
 
+
+//gives the server access to the public directory
+app.use(express.static('public'));
+
 app.use(morgan('dev'));
 
 //app.get("/", (req, res) => res.send("Hello World!"));
@@ -11,24 +15,74 @@ app.use(morgan('dev'));
 
 
 app.get("/", (req, res) => {  
-
   const posts = postBank.list();
-
   const html = `<!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-    </head>
-    <body>
-      <ul>
-        ${posts.map(post=>`<li>${ post.title + ' by ' + post.name }</li>`)}
-      </ul>
-    </body>
-    </html>`;
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      )}
+    </div>
+  </body>
+</html>`;
 
   res.send(html);
-  
 });
+
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+            <hr>
+            ${post.content}
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
+
+
+app.get('/users/:name', (req, res) => {
+  console.log( req.params.name );
+
+  res.send(req.params.name);
+});
+
 
 const PORT = 1337;
 
